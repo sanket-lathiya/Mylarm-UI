21a11b03933cbc22e9c3d2726d331c34c94b149a
Sanket-Lathiya <lsanket041@gmail.com>
2019-12-12 19:03:17 +0530

    Navbar setup

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/services/auth.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router'
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
 
-  constructor(private route: Router) { }
+  subsciption: Subscription;
+  isLogin: boolean = false;
+
+  constructor(private route: Router, private authService: AuthService) { }
 
   ngOnInit() {
+    this.subsciption = this.authService.isLoggedIn.subscribe(isLoggedIn => this.isLogin = isLoggedIn);
   }
 
+  logout() {
+    this.authService.logout();
+    this.route.navigate(['']);
+  }
+
+  ngOnDestroy(): void {
+    this.subsciption.unsubscribe();
+  }
 
 }
